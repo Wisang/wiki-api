d8523b20a2e71d814d3ef0a55caf329fb2532b7d
wisang <wseom7@gmail.com>
2022-09-28 23:36:22 +0900

    delete implemented with deleteOne

--- app.js
+++ app.js
@@ -110,6 +110,19 @@ app.route("/articles/:articleTitle")
         }
       }
     );
+  })
+
+  .delete(function(req,res){
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      function(err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("successfully deleted one");
+        }
+      }
+    );
   });
 
 app.listen(3000, function() {
